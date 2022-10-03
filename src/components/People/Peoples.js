5569a10f1a22a02ab243aded7a9de10380a0693b
Ankita Naik <ankitanaik471@gmail.com>
2022-10-03 23:29:04 +0530

    ADDED list items

--- src/components/People/Peoples.js
+++ src/components/People/Peoples.js
@@ -20,6 +20,12 @@ const people = [
       email: 'ted.fox@example.com',
       image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
     },
+    {
+      id: 'p_4',
+      name: 'Ted Fox',
+      email: 'ted.fox@example.com',
+      image: 'https://images.unsplash.com/photo-1500648767791-00dcc994a43e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    }
 ]
 
 
