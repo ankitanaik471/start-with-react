c4d27e9c0f4e7348440d41fa3b974bddf62ddd67
Ankita Naik <ankitanaik471@gmail.com>
2022-10-04 23:10:56 +0530

    First Event handler Added

--- src/components/UI/button/Button.js
+++ src/components/UI/button/Button.js
@@ -1,5 +1,9 @@
+const onClickEventHandler = () => {
+    console.log('button clicked')
+}
+
 const Button = (props) => {
-    return <button className="pointer-events-auto ml-8 rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">
+    return <button onClick={onClickEventHandler} className="pointer-events-auto ml-8 rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500">
                 {props.children}
             </button>
                          
