e5bf11869fcaf3faf2ce4f039bdb3d2b057eb71c
Ankita Naik <ankitanaik471@gmail.com>
2022-10-08 17:36:05 +0530

    form Input events

--- src/components/Products/CreateProduct/ProductForm.js
+++ src/components/Products/CreateProduct/ProductForm.js
@@ -1,24 +1,57 @@
 
 const ProductForm = () => {
+    const addProductNameInputHandler = (e) => {
+        console.log(e.target.value)
+    }
+
+    const addProductPriceInputHandler = (e) => {
+        console.log(e.target.value)
+    }
+
+    const addProductDescriptionInputHandler = (e) => {
+        console.log(e.target.value)
+    }
+
+    const avilabilityInputHandler = (e) => {
+        console.log(e.target.checked);
+    }
+
+    const imageInputHandler = (e) => {
+        console.log(e.target.value);
+    }
+
+    const addProductEventListner = () => {
+        console.log('clicked')
+    }
     return (
     <form>
         <div className="flex flex-col md:flex-row md:space-x-4 md:mb-4">
             <div className="md:basis-1/2">
                 <label className="mr-2">Product Name: </label>    
-                <input type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                <input onChange={addProductNameInputHandler} type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
             </div>
             <div className="md:basis-1/2">
-                <label className="mr-2">Product Name: </label>    
-                <input type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                <label className="mr-2">Product Price: </label>    
+                <input onChange={addProductPriceInputHandler} type="number" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
             </div>
         </div>
 
         <div className="md:mb-4">
             <label className="mr-2">Product Description: </label>    
-            <input type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+            <input onChange={addProductDescriptionInputHandler} type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
         </div>
 
-        <button className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500" type="submit">Submit</button>
+        <div className="md:mb-4">
+            <label className="mr-2">Is given product Available ? </label>    
+            <input onChange={avilabilityInputHandler} type="checkbox"  className="accent-pink-500"/>
+        </div>
+
+        <div className="md:mb-4">
+            <label className="mr-2">Select Product Image</label>    
+            <input onChange={imageInputHandler} type="file"/>
+        </div>
+
+        <button onClick={addProductEventListner} className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500" type="submit">Submit</button>
     </form>
         
     )
