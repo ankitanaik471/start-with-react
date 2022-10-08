4cc863f93aceb611b7cf31c54f2c124bf16c7d1d
Ankita Naik <ankitanaik471@gmail.com>
2022-10-08 20:02:01 +0530

    Adding multiple state in one component

--- src/components/Products/CreateProduct/ProductForm.js
+++ src/components/Products/CreateProduct/ProductForm.js
@@ -1,23 +1,30 @@
+import React, {useState} from "react";
 
 const ProductForm = () => {
+    let [name, setName] = useState('');
+    let [description, setDescription] = useState('');
+    let [price, setPrice] = useState('');
+    let [available, setAvailability] = useState('');
+    let [image, setImage] = useState('');
+
     const addProductNameInputHandler = (e) => {
-        console.log(e.target.value)
+        setName(e.target.value);
     }
 
     const addProductPriceInputHandler = (e) => {
-        console.log(e.target.value)
+        setPrice(e.target.value);
     }
 
     const addProductDescriptionInputHandler = (e) => {
-        console.log(e.target.value)
+        setDescription(e.target.value)
     }
 
     const avilabilityInputHandler = (e) => {
-        console.log(e.target.checked);
+        setAvailability(e.target.checked);
     }
 
     const imageInputHandler = (e) => {
-        console.log(e.target.value);
+        setImage(e.target.value);
     }
 
     const addProductEventListner = () => {
@@ -28,27 +35,27 @@ const ProductForm = () => {
         <div className="flex flex-col md:flex-row md:space-x-4 md:mb-4">
             <div className="md:basis-1/2">
                 <label className="mr-2">Product Name: </label>    
-                <input onChange={addProductNameInputHandler} type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                <input id={name} onChange={addProductNameInputHandler} type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
             </div>
             <div className="md:basis-1/2">
                 <label className="mr-2">Product Price: </label>    
-                <input onChange={addProductPriceInputHandler} type="number" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+                <input id={price} onChange={addProductPriceInputHandler} type="number" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
             </div>
         </div>
 
         <div className="md:mb-4">
             <label className="mr-2">Product Description: </label>    
-            <input onChange={addProductDescriptionInputHandler} type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
+            <input id={description} onChange={addProductDescriptionInputHandler} type="text" className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1" />
         </div>
 
         <div className="md:mb-4">
             <label className="mr-2">Is given product Available ? </label>    
-            <input onChange={avilabilityInputHandler} type="checkbox"  className="accent-pink-500"/>
+            <input id={available} onChange={avilabilityInputHandler} type="checkbox"  className="accent-pink-500"/>
         </div>
 
         <div className="md:mb-4">
             <label className="mr-2">Select Product Image</label>    
-            <input onChange={imageInputHandler} type="file"/>
+            <input id={image} onChange={imageInputHandler} type="file"/>
         </div>
 
         <button onClick={addProductEventListner} className="pointer-events-auto rounded-md bg-indigo-600 py-2 px-3 text-[0.8125rem] font-semibold leading-5 text-white hover:bg-indigo-500" type="submit">Submit</button>
